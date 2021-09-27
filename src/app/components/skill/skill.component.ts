ed5be5afffe5a0c76e27bee28beace9e70056b14
ananta580 <anantapoudel580@gmail.com>
2021-09-28 00:18:34 +0545

    Added Skill Page.

--- src/app/components/skill/skill.component.ts
+++ src/app/components/skill/skill.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Info from '../../../assets/Info.json';
 @Component({
   selector: 'pf-skill',
   templateUrl: './skill.component.html',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillComponent implements OnInit {
 
-  constructor() { }
+  skillAlternate = true;
+  expertiseOne: any;
+  expertiseTwo: any;
+  title: string | undefined;
+  body: string | undefined;
+  constructor() {
+    this.pullOnSkill();
+    this.expertiseOne = Info.SkillPage.ExpertiseOne;
+    this.expertiseTwo = Info.SkillPage.ExpertiseTwo;
+  }
 
   ngOnInit(): void {
   }
 
+  alterSkill() {
+    this.skillAlternate = !this.skillAlternate;
+  }
+  hoveringOnSkill(expertiseNo: number, index: number) {
+    if (expertiseNo === 1) {
+      this.title = this.expertiseOne.Skills[index].Name;
+      this.body = this.expertiseOne.Skills[index].Description;
+    }
+    else {
+      this.title = this.expertiseTwo.Skills[index].Name;
+      this.body = this.expertiseTwo.Skills[index].Description;
+    }
+  }
+  pullOnSkill() {
+    this.title = Info.SkillPage.Title;
+    this.body = Info.SkillPage.Description;
+    // this.body = `I am a <b>Software Engineer</b> with a motive of learning and growing through my experience. I have more than 2
+    // year experience with .Net and Angular, including my professional experience. I work as a full stack
+    // software developer, where I create Api using .Net as well as Other App using Razor + .Net and rest of time I
+    // spend with Angular to built amazing frontend using Frameworks like bootstrap, antdesign, material design.
+    // I have also hands on experience on Ionic and NativeScript to create Mobile Apps. Learning is my passion and
+    // I am countinously learning various JS Frontend Framework like Vuejs, Reactjs as well as Backend Frameworks
+    // like Django,Express etc.`;
+  }
 }
